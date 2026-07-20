c282e83f02ac8e180aac8205f449576fe0503a0f
bedanavrvm <bedanaurum99@gmail.com>
2026-07-20 16:42:43 +0300

    Add migrate directly to build.sh as a one-time measure

--- build.sh
+++ build.sh
@@ -10,6 +10,7 @@ pip install -r requirements.txt
 echo "Checking Django configuration and migrations..."
 python manage.py check
 python manage.py makemigrations --check --dry-run
+python manage.py migrate --noinput
 
 echo "Collecting static files..."
 python manage.py collectstatic --noinput
